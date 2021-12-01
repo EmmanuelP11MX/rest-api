06141e0458754481f8212096243285150bc9e6b2
EmmanuelP11MX <emmanuelpr10@gmail.com>
2021-11-30 20:26:17 -0600

    Metodos Creados

--- app/Controllers/Http/ProyectoController.js
+++ app/Controllers/Http/ProyectoController.js
@@ -1,6 +1,7 @@
 'use strict'
 
-const Proyecto = use ('App/Models/Proyecto')
+const Proyecto = use ('App/Models/Proyecto');
+const AutorizacionService = use ('App/Services/AutorizacionService');
 
 class ProyectoController {
     async index({ auth }) {
@@ -18,6 +19,26 @@ class ProyectoController {
         await user.proyectos().save(proyecto);
         return proyecto;
     }
+
+    async destroy({ auth, params}){
+        const user = await auth.getUser();
+        const { id } = params;
+        const proyecto = await Proyecto.find(id);
+        AutorizacionService.verificarPermiso(proyecto, user);
+        await proyecto.delete();
+        return proyecto;
+    }
+
+    async update ({ auth, params, request}) {
+        const user = await auth.getUser();
+        const { id } = params;
+        const proyecto = await Proyecto.find(id);
+        AutorizacionService.verificarPermiso(proyecto, user);
+        proyecto.merge(request.only('nombre'))
+        await proyecto.save();
+        return proyecto
+    }
+
 }
 
 module.exports = ProyectoController
