828852cfccd0a058c851187ef901dc93b371e9f6
EmmanuelP11MX <emmanuelpr10@gmail.com>
2021-12-01 23:14:47 -0600

    Creación de metodos path y delate

--- database/migrations/1638418452429_tarea_schema.js
+++ database/migrations/1638418452429_tarea_schema.js
@@ -4,11 +4,13 @@
 const Schema = use('Schema')
 
 class TareaSchema extends Schema {
+  
   up () {
     this.create('tareas',(table)=>{
       table.increments()
       table.integer('proyecto_id').unsigned().references('id').inTable('proyectos')
       table.string('descripcion',255).notNullable()
+      table.boolean('completada').defaultTo(false)
       table.timestamps()
     })
   }
